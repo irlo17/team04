53701757bf16b9b9e01414fddcc6a0f75caa9f39
irlo17 <iszrlo17@gmail.com>
2022-04-01 12:03:24 +0900

    로그아웃/회원탈퇴 기능 추가
    - 로그아웃하면 세션에 저장된 로그인 정보 삭제
    - 회원 탈퇴하면 DB 회원 레코드와 세션 로그인 정보 삭제
    - MemberController 주석 추가

--- src/main/java/com/team04/service/MemberService.java
+++ src/main/java/com/team04/service/MemberService.java
@@ -40,4 +40,9 @@ public interface MemberService {
 	 * - DB에 동일한 이메일을 가진 회원의 정보를 수정
 	 */
 	public int memberUpdate(MemberVO vo);
+	
+	/** 회원 탈퇴
+	 * 	- DB에 동일한 이메일과 패스워드를 가진 회원의 레코드를 삭제
+	 */
+	public int memberDelete(MemberVO vo);
 }
